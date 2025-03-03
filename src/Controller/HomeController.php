d37b6d36dcf069d55d4c9744e0d3e1b23d144505
audi0010 <tristan.audinot@etudiant.univ-reims.fr>
2025-03-04 00:15:10 +0100

    Modification de la landing page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\Tutoriel;
+use App\Repository\CategoryRepository;
+use App\Repository\TutorielRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,20 +11,11 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(CategoryRepository $categoryRepository, TutorielRepository $tutorielRepository): Response
     {
-        $categories = [
-            'Ouvrages préférés et favoris',
-            'Rechercher un ouvrage',
-            'Consulter un ouvrage et actions associées'
-        ];
-        $tutoriels = [
-            'Ajouter un livre à ses favoris',
-            'Consulter ses ouvrages favoris'
-        ];
         return $this->render('home/index.html.twig', [
-            'categories' => $categories,
-            'tutoriels' => $tutoriels,
+            'categories' => $categoryRepository->findAll(),
+            'tutoriels' => $tutorielRepository->findAll(),
         ]);
     }
 }
