56eb98cc7572ec8aaf59ffe2fc41299f5a15c63f
audi0010 <tristan.audinot@etudiant.univ-reims.fr>
2025-03-15 18:28:56 +0100

    Ajout du systeme de recherche

---
+++ public/assets/scripts/research.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const reseach = document.querySelector('input');
+    const listeCategories = document.querySelectorAll('.category');
+
+    reseach.addEventListener('change', () => {
+        listeCategories.forEach((category) => {
+            let hideParent = true;
+            category.querySelectorAll('.liste-tutoriels .tutoriel a').forEach((tutoriel) => {
+                if (!tutoriel.innerText.toLowerCase().includes(reseach.value.toLowerCase())) {
+                    tutoriel.parentElement.style.display = 'none'
+                } else {
+                    if (tutoriel.parentElement.style.display === 'none') {
+                        tutoriel.parentElement.style.display = 'block';
+                    }
+                }
+
+                if (tutoriel.parentElement.style.display === 'block') {
+                    hideParent = false;
+                }
+            })
+            if (hideParent) {
+                category.style.display = 'none';
+            } else {
+                if (category.style.display === 'none') {
+                    category.style.display = 'block';
+                }
+            }
+
+            if (category.querySelector('p').innerText.toLowerCase().includes(reseach.value.toLowerCase())
+                && category.style.display === 'none') {
+                category.style.display = 'block';
+                category.querySelectorAll('.liste-tutoriels .tutoriel').forEach((tutoriel) => {
+                    if (tutoriel.style.display === 'none') {
+                        tutoriel.style.display = 'block';
+                    }
+                })
+            }
+        })
+    })
+})
